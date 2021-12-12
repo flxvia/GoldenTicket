33aa26964df669a9496c7b68824307662b58ef2f
flxvia <flavia.mariasilvas25@gmail.com>
2021-12-12 02:39:57 -0300

    first commit

---
+++ src/golden/Golden.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package golden;
+
+import View.Splash;
+
+/**
+ *
+ * @author flavi
+ */
+public class Golden {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Splash().setVisible(true);
+    }
+    
+}
